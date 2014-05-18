b3ef491858c8f9118edcf3d25e8840156e98c017
Maxador <mongeaumax@gmail.com>
2014-05-18 17:34:23 -0400

    Function to add task with title

--- task/task.go
+++ task/task.go
@@ -1,2 +1,14 @@
 package task
 
+import "errors"
+
+type Task struct {
+	Title string
+}
+
+func NewTask(title string) (*Task, error) {
+	if title == "" {
+		return nil, errors.New("Empty title")
+	}
+	return &Task{title}, nil
+}
--- task/task_test.go
+++ task/task_test.go
@@ -2,9 +2,24 @@ package task
 
 import "testing"
 
-func TestNewTask( t *testing.T) {
-	task := NewTask("learn Go")
+func newTaskOrFatal(t *testing.T, title string) *Task {
+	task, err := NewTask(title)
+	if err != nil {
+		t.Fatalf("Unexpected error %v", err)
+	}
+	return task
+}
+
+func TestNewTask(t *testing.T) {
+	task := newTaskOrFatal(t, "learn Go")
 	if task.Title !=  "learn Go" {
 		t.Errorf("Expected learn Go, got %v", task.Title)
+	}
+}
+
+func TestNewTaskWithEmptyTitle(t *testing.T) {
+	_, err := NewTask("")
+	if err == nil {
+		t.Errorf("Expected 'Empty title' error, got %v", err)
 	}
 }
