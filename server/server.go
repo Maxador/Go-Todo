0fe0d7920bbce4985843425df6e36549f89ef640
Maxador <mongeaumax@gmail.com>
2014-05-21 07:53:44 -0400

    Started the server for route listening

--- server/server.go
+++ server/server.go
@@ -1,1 +1,26 @@
 package server
+
+import {
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/Maxador/Go-Todo/task"
+	"github.com/gorilla/mux"
+}
+
+var tasks = task.NewTaskManager()
+
+const PathPrefix = "/task/"
+
+func RegisterHandlers() {
+	r := mux.NewRouter()
+	r.HandleFunc(PathPrefix, errorHandler(ListTasks)).Methods("GET")
+}
+
+func ListTasks(w http.ResponseWriter, r *http.Request) error {
+	res := struct { Tasks []*task.Task }{tasks.All()}
+	return json.NewEncoder(w).Encode(res)
+}
